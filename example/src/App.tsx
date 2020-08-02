aeafd177b124f3a8bb16b342255cf4c17faa1b7e
Kyle Soeltz <wsoeltz@gmail.com>
2020-08-02 18:27:26 -0400

    Port usePrevious hook into module

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-
-import { ExampleComponent } from 'react-use-previous-hook'
+import React, {useState} from 'react'
+import usePrevious from 'react-use-previous-hook'
 import 'react-use-previous-hook/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [count, setCount] = useState<number>(0);
+
+  const previousCount = usePrevious(count);
+
+  return (
+    <>
+      <h1>Current count: {count}</h1>
+      <h1>Previous count: {previousCount}</h1>
+      <button onClick={() => setCount(count - 1)}>Count -</button>
+      <button onClick={() => setCount(count + 1)}>Count +</button>
+    </>
+  );
 }
 
 export default App
